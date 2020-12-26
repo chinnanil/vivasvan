650b4e2cb4d5870c0c56b2a6a87bb2cf9602a2c3
unknown <meghanilva@live.in>
2020-12-26 15:45:45 +0530

    done positioning header_left and header_right

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import  "./App.css";
 import image from "./images/main_image.jpg"
 
+{/* <img src={image} alt="nothing"></img> */}
 
 function App() {
   return (
    
       <div className="header">
-        <div className="header_left"><img src={image} alt="nothing"></img></div>
+        <div className="header_left"></div>
         <div className="header_right">2</div>
-        
+        <div id="content">jjkjjk</div>
       
     </div>
   );
