775a87a21e8ac1e5fa86d37c251faa74b9065028
unknown <meghanilva@live.in>
2020-12-28 15:23:03 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <div className="header_left"></div>
         <div className="header_right">
               <div className="title"><h2 className="tagLine">Official Agency for jlStream</h2>
-              <a target="_blank" href="" class="title__potluck-btn" rel="noopener noreferrer">Register with us →</a>
+              <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScuA6rnt8QH_Ke3QhobUcYQC53BbMyNru0Ic-i6lLWvUx97mg/viewform?usp=sf_link" class="title__potluck-btn" rel="noopener noreferrer">Register with us →</a>
               </div>
               <div className="people">
                 <Card />
