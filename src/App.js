e323f0640f0c891699d32ff9fb8059a1e2d620d3
unknown <meghanilva@live.in>
2020-12-29 17:53:05 +0530

    collage added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import  "./App.css";
-//import image from "./images/main_image.jpg";
+import image from "./images/main_image.jpg";
 import Card from "./components/Card";
+import col1 from "./images/collage/col1.jpg";
+import col2 from "./images/collage/col2.jpg";
+import  geeCropped  from "./images/gee_cropped.jpg";
+import col3 from "./images/collage/col3.jpg";
+import pixabay from "./images/pixabay.jpg";
+import myPost from "./images/collage2.jpg";
+
 
 
 
@@ -9,9 +16,14 @@ function App() {
   return (
    
       <div className="header">
-        <div className="header_left"></div>
+        <div className="header_left">
+      
+          <img src={myPost}></img>
+
+          
+        </div>
         <div className="header_right">
-              <div className="title"><h2 className="tagLine">Official Agency for jlStream</h2>
+              <div className="title"><h2 className="tagLine">Official Agency of jlStream</h2>
               <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScuA6rnt8QH_Ke3QhobUcYQC53BbMyNru0Ic-i6lLWvUx97mg/viewform?usp=sf_link" class="title__potluck-btn" rel="noopener noreferrer">Register with us →</a>
               </div>
               <div className="people">
